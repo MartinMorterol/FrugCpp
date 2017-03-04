b60c07c444051c462a9065f16000746674b74d4f
Your Name <you@example.com>
2017-03-04 10:46:34 +0100

    changement de structure

---
+++ conteneur/code/src/main.cpp
@@ -0,0 +1,25 @@
+#include <utility>
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <typeinfo>
+#include <fstream>
+#include <vector>
+#include <string>
+using std::vector;
+
+
+#include "lib/fonction_string.hpp"
+#include "lib/convertion.hpp"
+
+using namespace libRabbit;
+
+#include <list>
+
+
+int main(int argc, char** argv)
+{
+	cout << "Hello" << endl;	
+    
+   
+}
