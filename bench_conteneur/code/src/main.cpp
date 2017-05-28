7d92d1b965b3a1ca6f8368c397769c4d925189a0
MartinMorterol <martin.morterol@gmail.com>
2017-05-28 19:07:52 +0200

    j'ai oublier de commit

--- bench_conteneur/code/src/main.cpp
+++ bench_conteneur/code/src/main.cpp
@@ -20,6 +20,8 @@ using namespace libRabbit;
 
 #include "lib/tuple.hpp"
 #include "insertion_bench.hpp"
+#include "lecture_bench.hpp"
+
 #include "outil_bench.hpp"
 #include <boost/core/demangle.hpp>
 #include <list>
@@ -33,104 +35,71 @@ using namespace libRabbit;
 #include <iterator>
 #include <algorithm>
 
-template<class	T>
-void charger_data (T& conteneur, decltype(generate_lineaire(1))& data)
-{
-	std::copy(data.begin(), data.end(), std::inserter(conteneur,conteneur.begin()));
-}
 
-template<>
-void charger_data<std::map<size_t,size_t>> (std::map<size_t,size_t>& conteneur, decltype(generate_lineaire(1))& data)
+inline void read_for_auto (std::vector<size_t> vec)
 {
-	for (auto i : data) { conteneur[i] = i ;}
-}
-template<>
-void charger_data<std::unordered_map<size_t,size_t>> (std::unordered_map<size_t,size_t>& conteneur, decltype(generate_lineaire(1))& data)
-{
-	for (auto i : data) { conteneur[i] = i ;}
-}
-
-template<class T , class U = void>
-struct has_at : public std::false_type {};
-
-template<class T >
-struct has_at<T, void_if_valide_t<decltype(std::declval<T>().at(size_t()))> > : public std::true_type {};
-
-template <bool,class T>
-struct read_impl
-{
-	static void read(const T& conteneur, size_t taille)
+	size_t somme = 0;
+	for (const auto& elem : vec)
 	{
-		size_t cpt = 0;
-		std::vector<size_t> vec;
-		vec.reserve(taille);
-		for (const auto& elem : conteneur )
-		{
-			vec.push_back(elem);
-			if ( ++cpt >= taille) {
-				break;
-			}
-		}
+		somme += elem;
 	}
-};
-
-template <class T>
-struct read_impl<true, T>
-{
-	static void read(const T& conteneur, size_t taille)
-	{
-		std::vector<size_t> vec;
-		vec.reserve(taille);
-		for (size_t i = 0 ; i < std::min(taille,conteneur.size()) ; ++i )
-		{
-			vec.push_back(conteneur.at(i));
-		}
-	}
-};
-
-
-template <class T>
-void read(const T& conteneur, size_t taille)
-{
-	read_impl<has_at<T>::value,T>::read(conteneur,taille);
 }
 
+inline void read_for_max_out (std::vector<size_t> vec)
+{
+	size_t somme = 0;
+	size_t max = vec.size();
+	for (size_t i = 0 ; i < max ; ++i )
+	{
+		somme += vec[i];
+	}
+}
+
+inline void read_for_max_in (std::vector<size_t> vec)
+{
+	size_t somme = 0;
+	for (size_t i = 0 ; i < vec.size() ; ++i )
+	{
+		somme += vec[i];
+	}
+}
+
+inline void read_for_at_max_out (std::vector<size_t> vec)
+{
+	size_t somme = 0;
+	size_t max = vec.size();
+	for (size_t i = 0 ; i < max ; ++i )
+	{
+		somme += vec.at(i);
+	}
+}
 
 template <int max>
-void run_bench_read (std::string nom)
-{
-	auto data = generate_lineaire(10000000);
-	std::vector<size_t> vec;
-	std::list<size_t> list;
-	std::set<size_t> set;
-	std::map<size_t,size_t> map;
-	std::unordered_set<size_t> un_set;
-	std::unordered_map<size_t,size_t> un_map;
-
-	charger_data(vec,data);
-	charger_data(list,data);
-	charger_data(set,data);
-	charger_data(map,data);
-	charger_data(un_set,data);
-	charger_data(un_map,data);
-
-
-	auto plot = make_graphe(
-	    "list",		[&](size_t taille) { read(list,taille); },
-	    "vector",	[&](size_t taille) { read(vec,taille); },
-	    "un set",	[&](size_t taille) { read(un_set,taille); },
-	    "set",	[&](size_t taille) { read(set,taille); },
-	    "un map",	[&](size_t taille) { read(un_map,taille); },
-	    "map",	[&](size_t taille) { read(map,taille); }
-	);
-
-	for (size_t i = 10 ; i < max ; i*= 10 )
+	void run_vector_bench (std::string nom)
 	{
-		plot.run(i,i);
-	}
+		
+		std::vector<size_t> vec;
+		
+		
 
-	plot.generate_file("insertion_"+nom,"set logscale y","set key left top");
-}
+
+		auto plot = make_graphe(
+			"vector for auto",	[&]() { read_for_auto(vec); },
+			"vector for in",	[&]() { read_for_max_in(vec); },
+			"vector for out",	[&]() { read_for_max_out(vec); },
+			"vector for at out",	[&]() { read_for_at_max_out(vec); }
+		);
+		
+
+		for (size_t i = 10 ; i < max ; i*= 10 )
+		{
+			auto data = generate_lineaire(i);
+			charger_data(vec,data);
+			plot.run(i);
+		}
+
+		plot.generate_file("insertion_"+nom,"set logscale y","set key left top");
+	}
 
 
 int main()
@@ -138,7 +107,7 @@ int main()
 
 
 	constexpr size_t max = 10000000;
-	run_bench_read<max> ("lecture");
+	run_vector_bench<max> ("lecture");
 	/*run_bench_insert<10000000>("lineaire",generate_lineaire);
 	run_bench_insert<10000000>("random",generate_random);*/
 
--- bench_conteneur/code/src/outil_bench.hpp
+++ bench_conteneur/code/src/outil_bench.hpp
@@ -25,5 +25,37 @@ namespace libRabbit {
 		for (size_t i = 0 ; i < max ; ++i) { val.push_back(random(size_t(0),max)); }
 		return val;
 	}
+	
+	template<class	T>
+	void charger_data (T& conteneur, decltype(generate_lineaire(1))& data)
+	{
+		std::copy(data.begin(), data.end(), std::inserter(conteneur,conteneur.begin()));
+	}
+
+	template<>
+	void charger_data<std::map<size_t,size_t>> (std::map<size_t,size_t>& conteneur, decltype(generate_lineaire(1))& data)
+	{
+		for (auto i : data) { conteneur[i] = i ;}
+	}
+	template<>
+	void charger_data<std::unordered_map<size_t,size_t>> (std::unordered_map<size_t,size_t>& conteneur, decltype(generate_lineaire(1))& data)
+	{
+		for (auto i : data) { conteneur[i] = i ;}
+	}
+
+	template<class T , class U = void>
+	struct has_at : public std::false_type {};
+
+	template<class T >
+	struct has_at<T, void_if_valide_t<decltype(std::declval<T>().at(size_t()))> > : public std::true_type {};
+
+
+	template<class T , class U = void>
+	struct has_crochet : public std::false_type {};
+
+	template<class T >
+	struct has_crochet<T, void_if_valide_t<decltype(std::declval<T>()[size_t()])> > : public std::true_type {};
+	
+	
 }
 #endif // OUTIL_BENCH_HPP
